264de43296efa75663e9c790c1d34264c12a7ac7
Dmitry Bubnov <dbubnov@e1z2r7p24.42.us.org>
2019-10-01 16:10:55 -0700

    fdf

--- fdf.c
+++ fdf.c
@@ -3,17 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: metalium <metalium@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dbubnov <dbubnov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/01 09:54:50 by metalium          #+#    #+#             */
-/*   Updated: 2019/10/01 13:15:11 by metalium         ###   ########.fr       */
+/*   Updated: 2019/10/01 16:10:44 by dbubnov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int		main(int argc, char **argv)
+int	deal_key(int key, void *data)
 {
+	ft_printf("%c", key);
+	if (key == '5')
+		exit (0);
+	return (0);
+}
 
+int		main(void)
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1200, 1200, "mlx 42");
+	mlx_pixel_put(mlx_ptr, win_ptr, 200, 200, 0x34e8eb);
+	mlx_key_hook(win_ptr, deal_key, NULL); // NULL -> char map
+	mlx_loop(mlx_ptr);
+	
 	return (0);
 }
